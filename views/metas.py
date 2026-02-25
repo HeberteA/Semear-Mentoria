2caccf9c9378b96b6d8b2181c0964563755b16dd
HeberteA <heberte.2307@gmail.com>
2026-02-25 17:56:55 -0300

    Update metas.py

--- views/metas.py
+++ views/metas.py
@@ -4,6 +4,7 @@ import gspread
 from time import sleep
 from google.oauth2.service_account import Credentials
 
+@st.cache_data(ttl=600)
 def connect_to_sheets():
     scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
     credentials = Credentials.from_service_account_info(
