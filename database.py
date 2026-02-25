d855ba15e139b74f79b243f555c99b6b80cac29a
HeberteA <heberte.2307@gmail.com>
2026-02-25 18:08:32 -0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+
+@st.cache_resource
+def connect_to_sheets():
+    scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+    credentials = Credentials.from_service_account_info(
+        dict(st.secrets["gcp_service_account"]),
+        scopes=scopes
+    )
+    client = gspread.authorize(credentials)
+    return client.open("Semear Mentoria")
+
+@st.cache_data(ttl=600)
+def fetch_sheet_data(sheet_name):
+    sh = connect_to_sheets()
+    worksheet = sh.worksheet(sheet_name)
+    raw_data = worksheet.get_all_values()
+    
+    if not raw_data:
+        return pd.DataFrame()
+        
+    headers = [h.strip() for h in raw_data[0]]
+    return pd.DataFrame(raw_data[1:], columns=headers)
