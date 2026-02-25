0b38badd79b80397752ee3ce02b01753351e0afd
HeberteA <heberte.2307@gmail.com>
2026-02-25 17:53:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 
+@st.cache_resource
+def connect_to_sheets():
+    scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+    credentials = Credentials.from_service_account_info(
+        dict(st.secrets["gcp_service_account"]),
+        scopes=scopes
+    )
+    client = gspread.authorize(credentials)
+    return client.open("Semear Mentoria")
+
+@st.cache_data(ttl=300)
+def fetch_sheet_data(sheet_name):
+    sh = connect_to_sheets()
+    worksheet = sh.worksheet(sheet_name)
+    raw_data = worksheet.get_all_values()
+    
+    if not raw_data:
+        return pd.DataFrame()
+        
+    headers = [h.strip() for h in raw_data[0]]
+    return pd.DataFrame(raw_data[1:], columns=headers)
+
 st.set_page_config(
     page_title="Semear Mentoria",
     page_icon="logo.png",
@@ -150,6 +172,7 @@ def local_css():
 
 local_css()
 
+@st.cache_data(ttl=300)
 def get_all_students():
     try:
         scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
